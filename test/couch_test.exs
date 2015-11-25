71bb3e84db935f52d4bd2dcd5d8a715df8625916
argl <andi@null2.net>
2015-11-26 00:50:33 +0100

    added MIT copyright headers to sources

--- test/couch_test.exs
+++ test/couch_test.exs
@@ -164,7 +164,28 @@ defmodule Couch.Test do
     assert {:ok, _deleted} = result
   end
 
-  test "bulk save_docs, delete_docs", %{url: url, dbname: dbname} do
+  # Copyright (c) 2015 Andi Pieper
+
+  # Permission is hereby granted, free of charge, to any person obtaining a copy
+  # of this software and associated documentation files (the "Software"), to deal
+  # in the Software without restriction, including without limitation the rights
+  # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  # copies of the Software, and to permit persons to whom the Software is
+  # furnished to do so, subject to the following conditions:
+
+  # The above copyright notice and this permission notice shall be included in
+  # all copies or substantial portions of the Software.
+
+  # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+  # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  # THE SOFTWARE.
+
+
+test "bulk save_docs, delete_docs", %{url: url, dbname: dbname} do
     connection = Couch.server_connection url
     db = %Couch.DB{name: dbname, server: connection}
     docs = [%{_id: "test-document1", attr: "test"}, %{_id: "test-document2", attr: "test"}]
