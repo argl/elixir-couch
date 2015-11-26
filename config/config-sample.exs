246739cbbf15475ccb3356d43b52a8805c354ba3
argl <andi@null2.net>
2015-11-26 01:41:25 +0100

    reanmed sample environment config file

--- config/config-sample.exs
+++
@@ -1,3 +0,0 @@
-use Mix.Config
-
-config :couch, url: "http://localhost:5984"
---
+++ config/sample-environment.exs
@@ -0,0 +1,5 @@
+# Sample config for an environment. put this in test.exs, produciton exs and the like
+
+use Mix.Config
+
+config :couch, url: "http://localhost:5984"
